3ea4bed6a0d2a7c0670834091d26e904446f0eac
Antonio Manuel <agarlun2008@g.educaand.es>
2025-10-27 12:07:15 +0100

    EjerciciosPython(1-9)

---
+++ py001.py
@@ -0,0 +1,6 @@
+print("----- Lista de Tareas -----")
+tareas = ["Limpiar", "Comprar Comida", "Salir"]
+for indice in range(len(tareas)):
+    print(f"{indice + 1}. {tareas[indice]}")
+print("-----------------------------------")
+
