63f01e0d44fdec6addec1ef32c0ee0d2052dd48a
Antonio Manuel <agarlun2008@g.educaand.es>
2025-11-12 08:47:09 +0100

    Ejercicio 19-py0026

---
+++ py0026.py
@@ -0,0 +1,5 @@
+#Analiza el PY0025. ¿Qué líneas de código se deben cambiar si se usa set?
+# ¿Tendría sentido hacerlo con tuplas?
+
+# Las líneas de código que se deben cambiar son aquellas donde se crea la colección
+# de caracteres únicos a partir del texto ingresado.
